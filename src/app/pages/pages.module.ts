7670fbab58d70f6ad2efa16b2b29a9159aac311d
GuiHolanda <guilherme.lopes.holanda@hotmail.com>
2023-01-26 14:51:59 -0300

    26.01.2023_14.51

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { PagesRoutingModule } from './pages-routing.module';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, HomeComponent],
-  exports: [HeaderComponent, FooterComponent, HomeComponent],
+  declarations: [HomeComponent],
+  exports: [HomeComponent],
   imports: [CommonModule, PagesRoutingModule],
 })
 export class PagesModule {}
